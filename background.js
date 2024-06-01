eb0ec5fe2c963620554d69927a318c4b58f5431d
asrayg <asraygopa@gmail.com>
2024-06-01 17:33:18 -0500

    yay

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('YouTube to Slides Converter installed.');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'processVideo') {
+      fetch(`https://vercel.com/asraygs-projects/edu/process?url=${encodeURIComponent(message.url)}`)
+        .then(response => response.json())
+        .then(data => {
+          chrome.tabs.create({url: data.slidesUrl});
+        })
+        .catch(error => {
+          console.error('Error processing video:', error);
+          sendResponse({error: 'Failed to process video'});
+        });
+      return true;  // Will respond asynchronously
+    }
   });
   
